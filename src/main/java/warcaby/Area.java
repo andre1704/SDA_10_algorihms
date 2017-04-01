f459638357b6a196a5e4ad1f3c4f15b8f03c362e
andre1704 <andrzejslowinski93@gmail.com>
2017-04-01 11:11:47 +0200

    started 3.0

---
+++ src/main/java/warcaby/Area.java
@@ -0,0 +1,18 @@
+package warcaby;
+
+/**
+ * Created by RENT on 2017-03-29.
+ */
+public class Area {
+    Pole [][] plansza;
+    Area(){
+        plansza=new Pole[8][8];
+    }
+    public void polaUstawKolory(){
+
+
+
+    }
+
+
+}
